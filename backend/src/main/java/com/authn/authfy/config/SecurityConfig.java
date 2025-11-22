c41ceaa02e949f38ed7db3f601c7f8aede500b3d
Authn <authn.sapuarachchi@gmail.com>
2025-11-22 15:01:54 +0530

    change the AWS IP for retart instence

--- backend/src/main/java/com/authn/authfy/config/SecurityConfig.java
+++ backend/src/main/java/com/authn/authfy/config/SecurityConfig.java
@@ -44,8 +44,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
-                        ).permitAll()
+                                "/swagger-ui.html")
+                        .permitAll()
                         .requestMatchers("/login", "/register", "/send-reset-otp", "/verify-reset-otp",
                                 "/reset-password", "/logout", "/api/v1.0/login")
                         .permitAll()
@@ -71,7 +71,8 @@ public class SecurityConfig {
     private CorsConfigurationSource corsConfigurationSource() {
         // Implement your CORS configuration source here
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:5173", "http://18.139.217.48:3000"));
+        config.setAllowedOrigins(
+                List.of("http://localhost:3000", "http://localhost:5173", "http://18.139.117.10:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
--- backend/src/main/java/com/authn/authfy/config/WebConfig.java
+++ backend/src/main/java/com/authn/authfy/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Apply to all endpoints
-                        .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://18.139.217.48:3000") // Allow
+                        .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://18.139.117.10:3000") // Allow
                                                                                                                        // Frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these actions
                         .allowedHeaders("*")
